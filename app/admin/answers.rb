06bf98ba38823c286ed48c7c135e7f098b5bf0b9
MONARCHKOLI <monarchkoli12@gmail.com>
2023-05-26 11:32:48 +0530

    USER INTERFACE

--- app/admin/answers.rb
+++ app/admin/answers.rb
@@ -14,5 +14,15 @@ ActiveAdmin.register Answer do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+  form do |f|
+    inputs "Details" do
+      f.input :questionarie_id, as: :select, collection: Questionarie.all.collect{|question| [question.question, question.id]}
+      f.input :answer
+      f.input :correct
+      # f.input :user_id, as: :select, collection: User.all.collect{|user| [user.email,user.id]}
+    end
+    f.actions
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#index'
-  resources :homes
+  resources :homes, only: ['index']
   resources :banners do
-    resources :questionaries, only: ['show']
+    resources :questionaries, only: ['show'] do
+      resources :answers, only: ['show'] do
+        resources :user_responses
+      end
+    end
   end
+  
   # delete 'logout' => 'devise/sessions#destroy'
 end
